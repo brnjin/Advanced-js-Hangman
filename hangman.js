5cb7b0285e3cd183123cd67d0f6cdb9e8d4e68eb
Brian Jin <brnjin6@gmail.com>
2017-11-09 01:14:35 -0500

    Inquirer asks user to guess a letter

--- hangman.js
+++ hangman.js
@@ -1,8 +1,14 @@
 var inquirer = require("inquirer");
-var wordChoices = require("./word-bank.js");
-var lettersTransfer = require("./letter.js");
+/*var wordChoices = require("./word-bank.js");
+var lettersTransfer = require("./letter.js");*/
 
-
+inquirer.prompt({
+	type: "value",
+	name: "userGuess",
+	message: "Guess a letter!"
+}).then(function(guess) {
+	console.log(guess.userGuess);
+});
 //change words into blanks
 //number of wrongs counter
 //if win
